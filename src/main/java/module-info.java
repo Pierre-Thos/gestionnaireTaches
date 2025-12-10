eb246719fc7a6a5bab93906ff7d1142aad193776
nelson6541 <nelsonngoulou443@gmail.com>
2025-12-10 08:44:27 +0100

    https://github.com/Pierre-Thos/gestionnaireTaches.gitPages connexion et inscription

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,12 +5,7 @@ module fr.ece.gestionnairetaches {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
-    //requires fr.ece.gestionnairetaches;
-    requires javafx.graphics;
 
     opens fr.ece.gestionnairetaches to javafx.fxml;
-    opens fr.ece.gestionnairetaches.kanban to javafx.fxml;
-
-    exports fr.ece.gestionnairetaches.kanban;
-
+    exports fr.ece.gestionnairetaches;
 }
